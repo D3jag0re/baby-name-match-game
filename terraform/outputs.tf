efea09424d6b28faef328183d8d200621743ddf5
Mathew Brinkworth <mbrinkworth@venturiscc.com>
2024-07-03 10:34:48 -0400

    tf outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,1 +1,11 @@
-# Add here 
+output "storage_account_name" {
+  value = module.storage_account.storage_account_name
+}
+
+output "storage_account_container_name" {
+  value = module.storage_account.container_name_0
+}
+
+output "app_service_name" {
+  value = module.app_service_linux.app_service_name
+}

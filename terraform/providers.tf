e4d6a6032806e73526e2b46d7b029c9fa602ee96
Mathew Brinkworth <mbrinkworth@venturiscc.com>
2024-07-03 09:00:25 -0400

    added backend

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -5,7 +5,14 @@ terraform {
       version = "3.110.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name  = "tfstate-rg"
+    storage_account_name = "tfstatecacestorage"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 }
+
 
 provider "azurerm" {
   #skip_provider_registration = true 
